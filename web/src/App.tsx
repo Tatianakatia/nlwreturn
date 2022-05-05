fc023efe6fc3e34e9eb0f94408572b10530347e0
tatianakatia <tatiana.katia@gmail.com>
2022-05-06 00:26:38 +0100

    Impulse: Stage 2 - Created feedbackTypes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,13 @@
 import { Widget } from "./components/Widget";
+import { WidgetForm } from "./components/WidgetForm";
 
 export function App() {
-  return <Widget />
+  return (
+    <div>
+      <Widget />
+
+    </div>
+
+  )
+  
 }
