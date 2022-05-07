33bfbf159b1a176962901b1ddc765efb2cd02695
tatianakatia <tatiana.katia@gmail.com>
2022-05-07 17:58:53 +0100

    Commit fix scrollbar in textarea tag

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -51,7 +51,7 @@ export function FeedbackContentStep({
 
       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea 
-          className="min-w-[304px] w-full min-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin "
+          className="min-w-[304px] w-full min-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin "
           placeholder="Tell in detail what's going on..."
           onChange={event => setComment(event.target.value)}
         />
